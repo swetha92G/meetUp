46831660c18369ef4d50e60af3523666a65ddeb0
swetha92G <swethareddy.gillela@gmail.com>
2023-06-03 16:22:47 +0000

    meetUp

---
+++ src/App.js
@@ -0,0 +1,95 @@
+// import './App.css'
+
+// // These are the lists used in the application. You can move them to any component needed.
+// const topicsList = [
+//   {
+//     id: 'ARTS_AND_CULTURE',
+//     displayText: 'Arts and Culture',
+//   },
+//   {
+//     id: 'CAREER_AND_BUSINESS',
+//     displayText: 'Career and Business',
+//   },
+//   {
+//     id: 'EDUCATION_AND_LEARNING',
+//     displayText: 'Education and Learning',
+//   },
+//   {
+//     id: 'FASHION_AND_BEAUTY',
+//     displayText: 'Fashion and Learning',
+//   },
+//   {
+//     id: 'GAMES',
+//     displayText: 'Games',
+//   },
+// ]
+
+// // Replace your code here
+// const App = () => <h1>Hello World</h1>
+
+// export default App
+import {Component} from 'react'
+
+import {Route, Switch} from 'react-router-dom'
+import Register from './components/Register'
+import Home from './components/Home'
+
+import NotFound from './components/NotFound'
+
+import RegisterContext from './context/RegisterContext'
+
+import './App.css'
+
+// These are the lists used in the application. You can move them to any component needed.
+
+// Replace your code here
+class App extends Component {
+  state = {
+    name: '',
+    topic: 'Arts and Culture',
+    isRegistered: false,
+    showError: false,
+  }
+
+  changeName = name => {
+    this.setState({name})
+  }
+
+  changeTopic = topic => {
+    this.setState({topic})
+  }
+
+  registerName = () => {
+    this.setState({isRegistered: true})
+  }
+
+  updateError = () => {
+    this.setState({showError: true})
+  }
+
+  render() {
+    const {name, topic, isRegistered, showError} = this.state
+    return (
+      <RegisterContext.Provider
+        value={{
+          name,
+          topic,
+          isRegistered,
+          showError,
+          changeName: this.changeName,
+          changeTopic: this.changeTopic,
+          registerName: this.registerName,
+          updateError: this.updateError,
+        }}
+      >
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Register} />
+          <NotFound />
+        </Switch>
+      </RegisterContext.Provider>
+    )
+  }
+}
+
+export default App
